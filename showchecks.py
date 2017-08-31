30841ebf0d9b4e34c39165cd2783b5f32c7e92e9
xaviar orion <xaviar001@users.noreply.github.com>
2017-08-31 15:03:11 +0300

    Update showchecks.py

--- showchecks.py
+++ showchecks.py
@@ -5,7 +5,7 @@ app = Flask(__name__) #create an instance of the Flask library
 
 @app.route('/hello') #whenever this webserver is called with <hostname:port>/hello then this section is called
 def hello(): #The subroutine name that handles the call
-	output = 'Hello World'
+	output = 'Hello World Python'
 	return output #Whatever is returned from this subroutine is what is returned to the requester and is shown on the browser page
 
 if __name__ == '__main__':
